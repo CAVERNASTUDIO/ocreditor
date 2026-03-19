03c51c24129067a336186eeb20cae377203db64d
Erik García <91982607+CAVERNASTUDIO@users.noreply.github.com>
2026-03-19 15:50:24 -0600

    interfaceAppmenu
    Signed-off-by: Erik García <91982607+CAVERNASTUDIO@users.noreply.github.com>

--- OCRedit/x64/Bin/Scripts/EJ.bat
+++ OCRedit/x64/Bin/Scripts/EJ.bat
@@ -10,13 +10,19 @@ echo Tools
 echo ==============================
 echo 1. OCR Proyects
 echo 2. OCR Lock 
+echo 3. OCR Integrates Formats
+echo 4. OCR Tool
+echo 5. OCR Translator
 echo                             "e" to exit
 echo ==============================
-echo Copyright (c) - 2025 Erik Alejandro García Aparicio. all rigths reserved.
-set /p opcion=Selection (1-2): 
+echo Copyright (c) - 2025, 2026 Erik Alejandro García Aparicio. all rigths reserved.
+set /p opcion=Selection (1-5): 
 
 if "%opcion%"=="1" goto opcion1
 if "%opcion%"=="2" goto opcion2
+if "%opcion%"=="3" goto opcion3
+if "%opcion%"=="4" goto opcion4
+if "%opcion%"=="5" goto opcion5
 if "%opcion%"=="e" goto opcione
 
 echo Opción inválida. Intente de nuevo.
@@ -32,6 +38,24 @@ goto menu
 
 :opcion2
 python BloqueoDocumentos.py
+pause
+cls
+goto menu
+
+:opcion3
+python integra_formatos.py
+pause
+cls
+goto menu
+
+:opcion4
+python Pil.py
+pause
+cls
+goto menu
+
+:opcion5
+python Traslate.py
 pause
 cls
 goto menu
